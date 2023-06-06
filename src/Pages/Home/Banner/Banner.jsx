d83b3d2e392a2ae22f8dda4b25e652598ffa9405
Mahin Jasim <barakattanzil@gmail.comm>
2023-06-07 04:41:46 +0600

    navbar done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -7,9 +7,13 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './Banner.css';
 
-import banner1 from '../../../assets/banner-1jpg.jpg' ;
-import banner2 from '../../../assets/banner-2jpg.jpg' ;
-import banner3 from '../../../assets/cover-3jpg.jpg' ;
+import banner4 from '../../../assets/banner-1jpg.jpg' ;
+import banner1 from '../../../assets/banner-1jpg.png' ;
+import banner2 from '../../../assets/banner-2jpg.png' ;
+import banner3 from '../../../assets/cover-3jpg.png' ;
+import banner5 from '../../../assets/banner5jpg.jpg' ;
+import banner6 from '../../../assets/banner-6jpg.jpg' ;
+import banner7 from '../../../assets/banner-7jpg.jpg' ;
 
 const Banner = () => {
 
@@ -40,9 +44,13 @@ const Banner = () => {
                 <SwiperSlide><img src={banner1} alt="" /></SwiperSlide>
                 <SwiperSlide><img src={banner2} alt="" /></SwiperSlide>
                 <SwiperSlide><img src={banner3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={banner6} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={banner4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={banner5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={banner7} alt="" /></SwiperSlide>
                 <div className="autoplay-progress" slot="container-end">
                     <svg viewBox="0 0 48 48" ref={progressCircle}>
-                        <circle cx="24" cy="24" r="20"></circle>
+                        {/* <circle cx="24" cy="24" r="20"></circle> */}
                     </svg>
                     <span ref={progressContent}></span>
                 </div>
--- src/Shared/Navber.jsx
+++ src/Shared/Navber.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import logo from '../assets/dance-logo-dark-1.webp' ;
 
 
 const Navber = () => {
-    const user = false;
+    const user = true;
     const navOptions = <>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/menu">Our Menu</Link></li>
-        <li><Link to="/order/salad">Order Food</Link></li>
+        <li><Link to="/">Instructors</Link></li>
+        <li><Link to="/order/salad">Classes</Link></li>
         {
             user ? <>
+                <li><Link>Dashboard </Link></li>
                 <button className="btn btn-ghost">LogOut</button>
             </> : <>
                 <li><Link to="/login">Login</Link></li>
@@ -27,15 +29,13 @@ const Navber = () => {
                             {navOptions}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
+                    {/* <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a> */}
+                    <img src={logo} alt="" />
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         {navOptions}
                     </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Get started</a>
                 </div>
             </div>
         </>
