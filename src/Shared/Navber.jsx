02ab05ba7bd2f1bedd23d2c1f14daeefe180da27
Mahin Jasim <barakattanzil@gmail.comm>
2023-06-08 00:06:06 +0600

    navbar done with profile

--- src/Shared/Navber.jsx
+++ src/Shared/Navber.jsx
@@ -21,7 +21,6 @@ const Navber = () => {
             user ? <>
                 <li className="text-xl font-bold"><Link>Dashboard </Link></li>
                 <button onClick={handleLogOut} className="btn btn-ghost">LogOut</button>
-                <img className="w-16 h-16 rounded-full" src={user.photoURL} alt="" />
             </> : <>
                 <li className="text-xl font-bold"><Link to="/login">Login</Link></li>
             </>
@@ -30,13 +29,13 @@ const Navber = () => {
 
     return (
         <>
-            <div className="navbar mt-10  fixed z-10 bg-opacity-0 max-w-screen-xl ml-40 bg-black text-white">
+            <div className="navbar mt-10 flex items-center justify-between fixed z-10 bg-opacity-0 max-w-screen-2xl md:max-w-screen-xl md:ml-40 bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52">
                             {navOptions}
                         </ul>
                     </div>
@@ -48,6 +47,7 @@ const Navber = () => {
                         {navOptions}
                     </ul>
                 </div>
+                <img className="w-16 h-16 rounded-full" src={user?.photoURL} alt="" />
             </div>
         </>
     );
