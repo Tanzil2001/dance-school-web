8de0fe5226516c2ad345354a74d3324940ac3380
Mahin Jasim <barakattanzil@gmail.comm>
2023-06-08 03:55:59 +0600

    admin hook

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -1,43 +1,23 @@
-import { useContext, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../providers/AuthProvider';
+
+import { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { AuthContext } from "../Provider/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
 
 
-const useAxiosSecure = () => {
-    const navigate = useNavigate();
-    const { logOut } = useContext(AuthContext); 
-    const axiosSecure = axios.create({
-        baseURL: 'https://bistro-boos-server-tanzil2001.vercel.app'
-    });
+const useAdmin = () => {
+    const { user, loading } = useContext(AuthContext);
+    const [axiosSecure] = useAxiosSecure();
 
-    useEffect(() => {
-        axiosSecure.interceptors.request.use(
-            config => {
-                const token = localStorage.getItem('access-token');
-                if (token) {
-                    config.headers['Authorization'] = `Bearer ${token}`;
-                }
-                return config;
-            },
-            error => {
-                return Promise.reject(error);
-            }
-        );
-        axiosSecure.interceptors.response.use(
-            response => response,
-            error => {
-                if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-                    logOut().then(() => {
-                        navigate('/login'); 
-                    });
-                }
-                return Promise.reject(error);
-            }
-        );
-    }, [navigate, logOut, axiosSecure]);
-
-    return [axiosSecure];
+    const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
+        queryKey: ['isAdmin', user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/admin/${user?.email}`);
+            return res.data.admin;
+        }
+    })
+    return [isAdmin, isAdminLoading]
 };
 
-export default useAxiosSecure;
+export default useAdmin;
