7ec8fff20351887324dae29cdc95a73d009bfacf
Mahin Jasim <barakattanzil@gmail.comm>
2023-06-07 23:20:11 +0600

    login page design

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import cover1 from '../../assets/signup-cover.avif';
+import { AuthContext } from '../../Provider/AuthProvider';
+
 
 const Login = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const {signIn, googleSignIn} = useContext(AuthContext);
+
+    const glassStyle = {
+        backgroundColor: 'transparent',
+        backdropFilter: 'blur(5px)',
+        border: 'none',
+        boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)',
+        padding: '20px',
+        margin: '20px',
+    };
+
+    const onSubmit = data => {
+        console.log(data);
+        signIn(data.email, data.password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+
+    };
     return (
-        <div className='bg-black p-44'>
-            <div className='bg-blue-300 mt-6 text-white'>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minima non cumque maiores aperiam voluptatem dolor. Vel consequuntur minus voluptatibus, ratione ducimus blanditiis quibusdam. Ratione voluptate quod vitae nobis non?</p>
+        <div style={{ backgroundImage: `url(${cover1})` }} className="hero min-h-screen ">
+            <div className="hero-content mt-32">
+                <div style={glassStyle} className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-bold text-xl">Email</span>
+                            </label>
+                            <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered " />
+                            {errors.email && <span className="text-red-600">Email is required</span>}
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-bold text-xl">Password</span>
+                            </label>
+                            <input type="password"  {...register("password", {
+                                required: true,
+                            })} placeholder="password" className="input input-bordered " />
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary bg-transparent" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p className='text-xl font-bold'>Are You New Here? please <Link className='text-blue-600' to="/signup">Register</Link></p>
+                </div>
             </div>
-            <Link className='text-white' to="/signup">Register</Link>
         </div>
     );
 };
